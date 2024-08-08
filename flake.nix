0a648e5b0dd02d0bac41c8ff1630c7a23fc26eec
Stephen Price <stephen@stp5.net>
2024-08-08 13:12:38 -0700

    update flake

--- flake.nix
+++ flake.nix
@@ -1,23 +1,19 @@
 {
-  description =
-    "A Nix-flake-based development environment for Hugo";
+  description = "A Nix-flake-based development environment for Hugo";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    }:
-
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
           hugo
